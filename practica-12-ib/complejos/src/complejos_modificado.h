8d7893c187cc10e4b0d2fd425a1e426ff85789a5
Francisco <alu0101438412@ull.edu.es>
2021-01-11 13:33:48 +0000

    modificación pe_104

---
+++ practica-12-ib/complejos/src/complejos_modificado.h
@@ -0,0 +1,30 @@
+#ifndef COMPLEJOS_COMPLEJOS_SRC_H
+#define COMPLEJOS_COMPLEJOS_SRC_H
+#include <vector>
+class Complejo{
+    std::vector <float> n_complejo_;
+    public:
+        Complejo() : n_complejo_{0.0,0.0}{};
+        std::vector <float> AccederComplejo(){return(n_complejo_);};
+
+        bool ComprobarComplejo (std::vector <float> numero_complejo);
+
+        float ObtenerReal (std::vector <float> numero_complejo){if (ComprobarComplejo(numero_complejo)=true){return (numero_complejo.at(0));};
+
+        float ObtenerImaginaria (std::vector <float> numero_complejo){if (ComprobarComplejo(numero_complejo)=true){return (numero_complejo.at(1));};};
+
+        void Print (std::vector <float> numero_complejo);
+
+        std::vector <float> Add (std::vector <float> numero_complejo);
+
+        std::vector <float> Subtract (std::vector <float> numero_complejo);
+
+        bool operator-(const Complejo& numero_complejo, float numero){
+            Complejo complejo_resultado;
+            complejo_resultado.at(0)=numero + numero_complejo.at(0);
+            complejo_resultado.at(1)=numero_complejo.at(1);
+            return(complejo_resultado);
+        }
+           
+};
+#endif//Espera una declaración, aunque ; está colocado y el resto del header es correcto
